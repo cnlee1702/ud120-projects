92d7915ddc13ae544668cce18f82fbb179ac68b2
cnlee1702 <christinenicolelee@gmail.com>
2020-04-18 20:15:22 -0400

    init

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,25 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+"""
+    Credit to monkshow92 https://github.com/udacity/ud120-projects/issues/46
+"""
 
+original = "C:/Users/nal17/Documents/ud120-projects/final_project/final_project_dataset.pkl"
+destination = "C:/Users/nal17/Documents/ud120-projects/final_project/final_project_dataset_final.pkl"
 
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+enron_data = pickle.load(open('C:/Users/nal17/Documents/ud120-projects/final_project/final_project_dataset_final.pkl', "rb"))
